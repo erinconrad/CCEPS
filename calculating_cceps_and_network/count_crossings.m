32088d6bc19504de9a3e02733c51f3c7ead34523
Erin <erincconrad@gmail.com>
2021-08-05 16:48:41 -0400

    new way to detect ringing

--- calculating_cceps_and_network/count_crossings.m
+++ calculating_cceps_and_network/count_crossings.m
@@ -2,50 +2,48 @@ function out = count_crossings(eeg)
 
 out = 0;
 
-% Pick a few points to define signal end
-points = [1 length(eeg)];
 
-for ip = 1:length(points)
-    signal = eeg(points(ip,1):points(ip,2));
+signal = eeg;
 
-    % build new baseline (line connecting first and last point)
-    baseline = linspace(signal(1),signal(end),length(signal))';
 
-    % calculate signal minus baseline
-    sig_bl = signal-baseline; 
-    % when sig-bl = 0, signal is at baseline
-    % when sig-bl <0, < baseline
-    % when >0, > baseline
+% build new baseline (line connecting first and last point)
+baseline = linspace(nanmean(signal(1:round(0.1*length(eeg)))),...
+    nanmean(signal(round(0.9*length(eeg)):length(eeg))),length(signal))';
 
-    % get sign
-    Y = sign(sig_bl);
-    % 1 means signal above baseline
-    % 0 means signal = baseline
-    % -1 means signal < baseline;
+% calculate signal minus baseline
+sig_bl = signal-baseline; 
+% when sig-bl = 0, signal is at baseline
+% when sig-bl <0, < baseline
+% when >0, > baseline
 
-    % calculate difference between points
-    Ydiff = diff(Y);
-    % non-zero points are crossings (note that I will be double counting in the
-    % rare occasions in which Y is exactly 0, which only happens when the
-    % signal is exactly at baseline).
+% get sign
+Y = sign(sig_bl);
+% 1 means signal above baseline
+% 0 means signal = baseline
+% -1 means signal < baseline;
 
-    n_crossings = sum(Ydiff ~= 0);
-    
-    if n_crossings > out
-        out = n_crossings;
-    end
+% calculate difference between points
+Ydiff = diff(Y);
+% non-zero points are crossings (note that I will be double counting in the
+% rare occasions in which Y is exactly 0, which only happens when the
+% signal is exactly at baseline).
 
-    if 0
-        figure
+n_crossings = sum(Ydiff ~= 0);
 
-        plot(signal)
-        hold on
-        plot(baseline,'--')
-        title(sprintf('%d crossings',n_crossings))
-        pause
-        close(gcf)
-    end
-
+if n_crossings > out
+    out = n_crossings;
 end
+
+if 0
+    figure
+
+    plot(signal)
+    hold on
+    plot(baseline,'--')
+    title(sprintf('%d crossings',n_crossings))
+    pause
+    close(gcf)
+end
+
 
 end
