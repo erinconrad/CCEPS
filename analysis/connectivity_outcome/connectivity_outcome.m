f049f790c559ce8826076a65750b90acdf3fa63a
Erin <erincconrad@gmail.com>
2023-10-20 16:02:33 -0400

    connectivity analysis

--- analysis/connectivity_outcome/connectivity_outcome.m
+++ analysis/connectivity_outcome/connectivity_outcome.m
@@ -1,8 +1,8 @@
 %% relate connectivity to outcome
 clear
 
-% pick a single stim electrode
-stim_elec = 'LA1';
+% pick a stim electrode of interest
+stim_elecs = {'LA','LB','LC'};
 
 % file locs
 locations = cceps_files;
@@ -14,9 +14,9 @@ listing = dir([ccep_folder,'*HUP*.mat']); % hup patients
 nfiles = length(listing);
 
 % prepare variables
-name = {};
-avg_n1 = [];
-avg_n2 = [];
+name = cell(nfiles,1);
+avg_n1 = nan(nfiles,1);
+avg_n2 = nan(nfiles,1);
 
 for i = 1:nfiles
     
@@ -24,28 +24,36 @@ for i = 1:nfiles
     out = load([ccep_folder,listing(i).name]);
     out = out.out;
 
-    % find index of chosen electrode
-    chosen_idx = strcmp(out.chLabels,stim_elec);
-
-    % skip if the patient doesn't have it
-    if sum(chosen_idx) == 0, continue; end
-
-    % skip if we didn't stim it
-    if out.stim_chs(chosen_idx) == 0, continue; end
-
-    % if made it to here, include this patient
+    % patient name
     C = strsplit(out.name,'_');
     hup_name = C{1};
     name = [name;hup_name];
 
-    % get the average connectivity between this chosen contact and other
-    % contacts on the same electrode
-    other_contacts = contains(out.chLabels,stim_elec(1:2));
-    n1 = out.network(1).A(chosen_idx,other_contacts);
-    n2 = out.network(2).A(chosen_idx,other_contacts);
+    % loop over electrodes
+    n1s = cell(length(stim_elecs),1);
+    n2s = cell(length(stim_elecs),1);
+    for is = 1:length(stim_elecs)
 
-    avg_n1 = [avg_n1;nanmean(n1)];
-    avg_n2 = [avg_n2;nanmean(n2)];
+        % find all contacts on that electrode
+        idx = contains(out.chLabels,stim_elecs{is});
+
+        % get the avg connectivity
+        n1s{is} = out.network(1).A(idx,idx);
+        n2s{is} = out.network(2).A(idx,idx);
+
+    end
+
+    % flatten the arrays
+    all_n1s = [];
+    all_n2s = [];
+    for is = 1:length(stim_elecs)
+        all_n1s = [all_n1s;reshape(n1s{is},size(n1s{is},1)*size(n1s{is},1),1)];
+        all_n2s = [all_n2s;reshape(n2s{is},size(n2s{is},1)*size(n2s{is},1),1)];
+    end
+   
+
+    avg_n1 = [avg_n1;nanmean(all_n1s)];
+    avg_n2 = [avg_n2;nanmean(all_n2s)];
     
 end
 
