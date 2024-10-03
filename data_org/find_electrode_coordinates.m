538b08ca2f092fc883e444c0cde4792559332040
Erin <erincconrad@gmail.com>
2024-10-03 12:06:55 -0400

    finding cordinates

--- data_org/find_electrode_coordinates.m
+++ data_org/find_electrode_coordinates.m
@@ -5,7 +5,8 @@
 patient_list_file = '../../data/CNTSurgicalRepositor-Erinprotocolstimr01_DATA_LABELS_2024-10-01_1157.csv';
 
 % parent directory for imaging data
-parent_dir = '/mnt/leif/littlab/data/Human_Data/CNT_iEEG_BIDS/';
+parent_dir1 = '/mnt/leif/littlab/data/Human_Data/CNT_iEEG_BIDS/';
+parent_dir2 = '/mnt/leif/littlab/data/Human_Data/recon/BIDS_penn/';
 
 % output
 output_folder = '../../cceps_results/elec_info/';
@@ -24,11 +25,22 @@ for r = 1:size(T,1)
     else
         rids = sprintf('%d',rid);
     end
-    sub_dir = [parent_dir,'sub-RID',rids,'/derivatives/ieeg_recon/module3/'];
-    coor_file = '*atropos*csv';
-    full_file = [sub_dir,coor_file];
-    listing = dir(full_file);
 
+    coor_file = '*atropos*csv';
+
+    sub_dir1 = [parent_dir1,'sub-RID',rids,'/derivatives/ieeg_recon/module3/']; 
+    full_file1 = [sub_dir1,coor_file];
+    listing1 = dir(full_file1);
+
+    sub_dir2 = [parent_dir2,'sub-RID',rids,'/derivatives/ieeg_recon/module3/'];
+    full_file2 = [sub_dir2,coor_file];
+    listing2 = dir(full_file1);
+
+    if length(listing1) >0
+        listing = listing1;
+    else
+        listing = listing2;
+    end
 
     if length(listing) == 0
         fprintf('\nWarning, can''t find file for rid %d\n',rid);
